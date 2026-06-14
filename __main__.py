be2a6960c4cf673f31ba2321eb75fc8e77e31e1b
Santiago Cobas <aquaticsanti@gmail.com>
2026-06-13 23:46:43 -0300

    Made the outline for the title screen

--- __main__.py
+++ __main__.py
@@ -1,4 +1,46 @@
-import rich
+from rich import box
+from rich.console import Console
+from rich.table import Table
 from readchar import key, readkey
+import time
 
-rich.print("[bold]UNDER CONSTRUCTION[/bold]")
+
+
+width = 9
+height = 9
+mines = 10
+
+console = Console()
+
+def printTitle():
+    global console, width, height, mines
+    console.print("""
+█▀▀█  █    ▀█▀      █▀▄▀█  ▀  █▀▀▄ █▀▀ █▀▀ 
+█     █     █   ▀▀  █ █ █ ▀█▀ █  █ █▀▀ ▀▀█ 
+█▄▄█  █▄▄█ ▄█▄      █   █ ▀▀▀ ▀  ▀ ▀▀▀ ▀▀▀ \n\n"""
+                , style="bold default on default", justify="center")
+    
+    console.print(f" Width  = {width}", justify="center")
+    console.print(f" Height = {height}", justify="center")
+    console.print(f" Mines  = {mines}", justify="center")
+    console.print("""
+╔════════════════╗
+║     [bold]Start![/bold]     ║
+╚════════════════╝""", markup=True, justify="center")
+
+
+
+printTitle()
+
+# For later!
+#table = Table(title="", box=box.SQUARE, show_lines=True)
+#
+#for i in range(width):
+#    table.add_column(width=2)
+#
+#for i in range(height-1):
+#    table.add_row()
+#
+#console.print(table)
+
+

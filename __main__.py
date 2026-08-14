08b14b469075dec63c00f67a87c5e89f00bf2bfb
Santiago Cobas <aquaticsanti@gmail.com>
2026-08-13 21:16:37 -0300

    Added an option to disable sounds

--- __main__.py
+++ __main__.py
@@ -17,6 +17,7 @@ def cls():
 width = 9
 height = 9
 mines = 10
+soundOn = True
 
 # Style definition
 class styles:
@@ -72,8 +73,18 @@ def printTitle():
         console.print(f" Mines  = {mines}", justify="center", style="black on white", highlight=False)
     else:
         console.print(f" Mines  = {mines}", justify="center", highlight=False)
-    print("\n")
     if selected == 4:
+        if soundOn:
+            console.print(f" Sound  = On", justify="center", style="black on white", highlight=False)
+        else:
+            console.print(f"  Sound  = Off", justify="center", style="black on white", highlight=False)
+    else:
+        if soundOn:
+            console.print(f" Sound  = On", justify="center", highlight=False)
+        else:
+            console.print(f"  Sound  = Off", justify="center", highlight=False)
+    print("\n")
+    if selected == 5:
         console.print("""╔════════════════╗
 ║     Start!     ║
 ╚════════════════╝""", markup=True, justify="center", style="black on white", highlight=False)
@@ -90,12 +101,12 @@ while True:
     k = readkey()
     if k == key.DOWN:
         selected += 1
-        if selected > 4:
+        if selected > 5:
             selected = 1
     elif k == key.UP:
         selected -= 1
         if selected < 1:
-            selected = 4
+            selected = 5
     elif k == key.LEFT:
         if selected == 1:
             width -= 1
@@ -109,6 +120,11 @@ while True:
             mines -= 1
             if mines < 1:
                 mines += 1
+        elif selected == 4:
+            if soundOn:
+                soundOn = False
+            else:
+                soundOn = True
         if (width * height) < mines:
             mines = width * height
     elif k == key.RIGHT:
@@ -120,10 +136,19 @@ while True:
             mines += 1
             if mines > (height * width):
                 mines -= 1
-    elif k == key.ENTER and selected == 4:
+        elif selected == 4:
+            if soundOn:
+                soundOn = False
+            else:
+                soundOn = True
+    elif k == key.ENTER and selected == 5:
         break
 
-
+if soundOn == False:
+    del playsound
+    def playsound(path, block=False):
+        """Does nothing. This is a dummy function that replaces playsound from playsound3 if sound is disabled."""
+        return
 """
 _infoGrid_ guide:
 -1 = Mine
@@ -149,10 +174,38 @@ _flagGrid guide:
 """
 flagGrid = np.zeros((width, height), dtype=int) # Array that holds which cells have been flagged
 
+"""
+_outcome_ guide:
+0 = Game in progress/not started
+1 = Won
+-1 = Lost
+"""
+outcome = 0 # Variable that holds outcome of game
+
+"""
+_mineGrid_ guide:
+0 = No mine here
+1 = Mine here
+(Just an array to make checking flagged mines easier, copy of infoGrid)
+Not defined here.
+"""
+
 def printGrid():
-    global grid, thisRow, width, height, selected
+    global grid, thisRow, width, height, selected, minesLeft, mines, flagGrid, outcome
     thisRow = []
     grid = Table.grid()
+    if outcome == 0:
+        try:
+            minesLeft = mines - np.count_nonzero(flagGrid)
+        except NameError:
+            minesLeft = mines
+        if minesLeft >= 0:
+            grid.title = f"[bold]{minesLeft}[/bold] mines left!"
+        else:
+            grid.title = f"[bold]{minesLeft}[/bold] mines left?"
+    elif outcome == 1:
+        grid.title = f"[bold]0[/bold] mines left!\n[bold][underline]You've Won![/underline][/bold]"
+        grid.caption = "[italic]Press Enter to exit[/italic]"
     for x in range(height):
         for y in range(width):
             if selected == (y, x):
@@ -216,86 +269,109 @@ def discover_and_adjacents(y, x):
 
 selected = (0, 0)
 cls()
-with Live(Align.center(printGrid()), refresh_per_second=30) as live:
+with Live(Align.center(printGrid()), refresh_per_second=30, console=console) as live:
     while True:
-        k = readkey()
-        if k == key.RIGHT:
-            selected = (selected[0]+1, selected[1])
-            if selected[0] > width-1:
-                selected = (selected[0]-1, selected[1])
-        elif k == key.LEFT:
-            selected = (selected[0]-1, selected[1])
-            if selected[0] < 0:
+        if outcome == 0:
+            k = readkey()
+            if k == key.RIGHT:
                 selected = (selected[0]+1, selected[1])
-        elif k == key.UP:
-            selected = (selected[0], selected[1]-1)
-            if selected[1] < 0:
-                selected = (selected[0], selected[1]+1)
-        elif k == key.DOWN:
-            selected = (selected[0], selected[1]+1)
-            if selected[1] > height-1:
+                if selected[0] > width-1:
+                    selected = (selected[0]-1, selected[1])
+            elif k == key.LEFT:
+                selected = (selected[0]-1, selected[1])
+                if selected[0] < 0:
+                    selected = (selected[0]+1, selected[1])
+            elif k == key.UP:
                 selected = (selected[0], selected[1]-1)
-        elif k == key.BACKSPACE:
-            if discoveryGrid[(selected[0], selected[1])] == 0:
-                playsound("sfx/flag.wav", block=False)
-                if flagGrid[(selected[0], selected[1])] == 0:
-                    flagGrid[(selected[0], selected[1])] = 1
-                else:
-                    flagGrid[(selected[0], selected[1])] = 0
-            else:
-                pass
-        elif k == key.ENTER:
-            if flagGrid[(selected[0], selected[1])] == 1:
-                continue
-            if firstClick == True:
-                firstClick = False
-                safe = [] # NOTE: From this line until the 13 next AI was used. 
-                for dy in [-1, 0, 1]:
-                    for dx in [-1, 0, 1]:
-                        ny = selected[0] + dy
-                        nx = selected[1] + dx
-                        if 0 <= ny < width and 0 <= nx < height:
-                            safe.append(ny * height + nx)
-                positions = []
-                for i in range(len(flat)):
-                    if i not in safe:
-                        positions.append(i)
-                values = flat[positions]
-                np.random.shuffle(values)
-                flat[positions] = values
-                infoGrid = flat.reshape(width, height)
-                for i in range(width):
-                    for j in range(height):
-                        if infoGrid[(i, j)] != -1:
+                if selected[1] < 0:
+                    selected = (selected[0], selected[1]+1)
+            elif k == key.DOWN:
+                selected = (selected[0], selected[1]+1)
+                if selected[1] > height-1:
+                    selected = (selected[0], selected[1]-1)
+            elif k == key.BACKSPACE:
+                if discoveryGrid[(selected[0], selected[1])] == 0:
+                    playsound("sfx/flag.wav", block=False)
+                    
+                    if flagGrid[(selected[0], selected[1])] == 0:
+                        flagGrid[(selected[0], selected[1])] = 1
+                        try:
+                            if np.array_equal(flagGrid, mineGrid):
+                                    outcome = 1
+                        except NameError:
                             pass
-                        else:
-                            if j+1 <= height-1:
-                                if infoGrid[(i, j+1)] > -1:
-                                    infoGrid[(i, j+1)] += 1
-                            if j-1 >= 0:
-                                if infoGrid[(i, j-1)] > -1:
-                                    infoGrid[(i, j-1)] += 1
-                            if i+1 <= width-1:
-                                if infoGrid[(i+1, j)] > -1:
-                                    infoGrid[(i+1, j)] += 1
-                            if i-1 >= 0:
-                                if infoGrid[(i-1, j)] > -1:
-                                    infoGrid[(i-1, j)] += 1
-                            if i-1 >= 0 and j-1 >= 0:
-                                if infoGrid[(i-1, j-1)] > -1:
-                                    infoGrid[(i-1, j-1)] += 1
-                            if i+1 <= width-1 and j+1 <= height-1:
-                                if infoGrid[(i+1, j+1)] > -1:
-                                    infoGrid[(i+1, j+1)] += 1
-                            if i+1 <= width-1 and j-1 >= 0:
-                                if infoGrid[(i+1, j-1)] > -1:
-                                    infoGrid[(i+1, j-1)] += 1
-                            if i-1 >= 0 and j+1 <= height-1:
-                                if infoGrid[(i-1, j+1)] > -1:
-                                    infoGrid[(i-1, j+1)] += 1
-            discover_and_adjacents(selected[0], selected[1])
-            if infoGrid[(selected[0], selected[1])] == -1:
-                playsound("sfx/explosion.wav", block=False)
-            else:
-                playsound("sfx/shovel.wav", block=False)
-        live.update(Align.center(printGrid()))
+                    else:
+                        flagGrid[(selected[0], selected[1])] = 0
+                else:
+                    pass
+            elif k == key.ENTER:
+                if flagGrid[(selected[0], selected[1])] == 1:
+                    continue
+                if firstClick == True:
+                    firstClick = False
+                    safe = [] # NOTE: From this line until the 13 next AI was used. 
+                    for dy in [-1, 0, 1]:
+                        for dx in [-1, 0, 1]:
+                            ny = selected[0] + dy
+                            nx = selected[1] + dx
+                            if 0 <= ny < width and 0 <= nx < height:
+                                safe.append(ny * height + nx)
+                    positions = []
+                    for i in range(len(flat)):
+                        if i not in safe:
+                            positions.append(i)
+                    values = flat[positions]
+                    np.random.shuffle(values)
+                    flat[positions] = values
+                    infoGrid = flat.reshape(width, height)
+                    mineGrid = infoGrid.copy()
+                    for h in range(height):
+                        for w in range(width):
+                            try:
+                                if mineGrid[(h, w)] != -1:
+                                    mineGrid[(h, w)] = 0
+                            except IndexError: # I honestly don't know but it happened
+                                pass
+                    for h in range(height):
+                        for w in range(width):
+                            try:
+                                if mineGrid[(h, w)] == -1:
+                                    mineGrid[(h, w)] = 1
+                            except IndexError: # I honestly don't know but it happened
+                                pass
+                    for i in range(width):
+                        for j in range(height):
+                            if infoGrid[(i, j)] != -1:
+                                pass
+                            else:
+                                if j+1 <= height-1:
+                                    if infoGrid[(i, j+1)] > -1:
+                                        infoGrid[(i, j+1)] += 1
+                                if j-1 >= 0:
+                                    if infoGrid[(i, j-1)] > -1:
+                                        infoGrid[(i, j-1)] += 1
+                                if i+1 <= width-1:
+                                    if infoGrid[(i+1, j)] > -1:
+                                        infoGrid[(i+1, j)] += 1
+                                if i-1 >= 0:
+                                    if infoGrid[(i-1, j)] > -1:
+                                        infoGrid[(i-1, j)] += 1
+                                if i-1 >= 0 and j-1 >= 0:
+                                    if infoGrid[(i-1, j-1)] > -1:
+                                        infoGrid[(i-1, j-1)] += 1
+                                if i+1 <= width-1 and j+1 <= height-1:
+                                    if infoGrid[(i+1, j+1)] > -1:
+                                        infoGrid[(i+1, j+1)] += 1
+                                if i+1 <= width-1 and j-1 >= 0:
+                                    if infoGrid[(i+1, j-1)] > -1:
+                                        infoGrid[(i+1, j-1)] += 1
+                                if i-1 >= 0 and j+1 <= height-1:
+                                    if infoGrid[(i-1, j+1)] > -1:
+                                        infoGrid[(i-1, j+1)] += 1
+                discover_and_adjacents(selected[0], selected[1])
+                if infoGrid[(selected[0], selected[1])] == -1:
+                    playsound("sfx/explosion.wav", block=False)
+                    outcome = -1
+                else:
+                    playsound("sfx/shovel.wav", block=False)
+            live.update(Align.center(printGrid()))
